4335fccdbf5f9e4568adfd118ba3126e87e67522
MarioA96 <marioaquirozs@gmail.com>
2024-10-21 16:03:41 -0600

    Se configuro el archivo base de astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://marioa96.github.io',
-    base: '09-FEM_multiStepForm',
+    base: '03_FEM_socialLinksProfile',
 });
