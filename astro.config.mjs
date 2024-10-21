84987d7675df42ca34872199dc16b0dd4b4e877f
MarioA96 <marioaquirozs@gmail.com>
2024-10-21 15:57:45 -0600

    Se realizaron cambios en la redacción del README

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://MarioA96.github.io',
+    site: 'https://marioa96.github.io',
     base: '09-FEM_multiStepForm',
 });
